b5965f9ac2c3ea21c131eb22eb08ad9204bb6854
Tatiana <tanyagavrilenko08@gmail.com>
2020-10-01 21:10:35 +0300

    Update

---
+++ build/precache-manifest.2213d02bd8f7261f7d861cc05ddc7630.js
@@ -0,0 +1,62 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "a7d34de39fb44e23e460e7c5210866fc",
+    "url": "/index.html"
+  },
+  {
+    "revision": "f2ed1e4618d93de07daf",
+    "url": "/static/css/2.4707e12a.chunk.css"
+  },
+  {
+    "revision": "73b73703d283087ddf6a",
+    "url": "/static/css/main.b82a23dc.chunk.css"
+  },
+  {
+    "revision": "f2ed1e4618d93de07daf",
+    "url": "/static/js/2.77d8015b.chunk.js"
+  },
+  {
+    "revision": "a1a636632721a402612df3e043dd630d",
+    "url": "/static/js/2.77d8015b.chunk.js.LICENSE.txt"
+  },
+  {
+    "revision": "73b73703d283087ddf6a",
+    "url": "/static/js/main.217ebefa.chunk.js"
+  },
+  {
+    "revision": "9b4a64e80bb00e31c599",
+    "url": "/static/js/runtime-main.01d603b4.js"
+  },
+  {
+    "revision": "bc316abc7e519ee1c946ca5205f1b4ec",
+    "url": "/static/media/contact-us.bc316abc.jpg"
+  },
+  {
+    "revision": "ad0a535af0148b8d7f333505e5152171",
+    "url": "/static/media/popup-contact.ad0a535a.jpg"
+  },
+  {
+    "revision": "a15a0726b0783a29b529bc4edbf5d1b1",
+    "url": "/static/media/property-bgi.a15a0726.jpg"
+  },
+  {
+    "revision": "d12a5a6b6538c94cc4fd7cd4a20806b4",
+    "url": "/static/media/s1.d12a5a6b.jpg"
+  },
+  {
+    "revision": "84df357d788eca9714bff508794f26ac",
+    "url": "/static/media/s2.84df357d.jpg"
+  },
+  {
+    "revision": "ca020828ba7f43808710e731e60fd6cd",
+    "url": "/static/media/s3.ca020828.jpg"
+  },
+  {
+    "revision": "750124eeaa26ccb61e6cd0eb86f642d7",
+    "url": "/static/media/s4.750124ee.jpg"
+  },
+  {
+    "revision": "300c205ae0ad6bf455eb4ad04628244e",
+    "url": "/static/media/s5.300c205a.jpg"
+  }
+]);

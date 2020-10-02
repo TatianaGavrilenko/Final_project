4aa66bbe5b7b87dbf96771600c20ca97f4fe0ab6
Tatiana <tanyagavrilenko08@gmail.com>
2020-10-02 10:52:50 +0300

    Build

---
+++ build/precache-manifest.5738b17d50ef2f554ca658dbbba18b4c.js
@@ -0,0 +1,66 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "03938a25d94a7436853772df9dddb8ab",
+    "url": "/Final_Project/index.html"
+  },
+  {
+    "revision": "04a24479617cd52ce2cb",
+    "url": "/Final_Project/static/css/2.4707e12a.chunk.css"
+  },
+  {
+    "revision": "0fc84741430a5d7f6e75",
+    "url": "/Final_Project/static/css/main.05a5b8e2.chunk.css"
+  },
+  {
+    "revision": "04a24479617cd52ce2cb",
+    "url": "/Final_Project/static/js/2.d529afdc.chunk.js"
+  },
+  {
+    "revision": "a1a636632721a402612df3e043dd630d",
+    "url": "/Final_Project/static/js/2.d529afdc.chunk.js.LICENSE.txt"
+  },
+  {
+    "revision": "0fc84741430a5d7f6e75",
+    "url": "/Final_Project/static/js/main.a5e972af.chunk.js"
+  },
+  {
+    "revision": "72d579826bb717735fc0",
+    "url": "/Final_Project/static/js/runtime-main.e9682e6f.js"
+  },
+  {
+    "revision": "0d1405e6dedd0e1e70871fae9229f9ee",
+    "url": "/Final_Project/static/media/1(3).0d1405e6.jpg"
+  },
+  {
+    "revision": "bc316abc7e519ee1c946ca5205f1b4ec",
+    "url": "/Final_Project/static/media/contact-us.bc316abc.jpg"
+  },
+  {
+    "revision": "ad0a535af0148b8d7f333505e5152171",
+    "url": "/Final_Project/static/media/popup-contact.ad0a535a.jpg"
+  },
+  {
+    "revision": "a15a0726b0783a29b529bc4edbf5d1b1",
+    "url": "/Final_Project/static/media/property-bgi.a15a0726.jpg"
+  },
+  {
+    "revision": "d12a5a6b6538c94cc4fd7cd4a20806b4",
+    "url": "/Final_Project/static/media/s1.d12a5a6b.jpg"
+  },
+  {
+    "revision": "84df357d788eca9714bff508794f26ac",
+    "url": "/Final_Project/static/media/s2.84df357d.jpg"
+  },
+  {
+    "revision": "ca020828ba7f43808710e731e60fd6cd",
+    "url": "/Final_Project/static/media/s3.ca020828.jpg"
+  },
+  {
+    "revision": "750124eeaa26ccb61e6cd0eb86f642d7",
+    "url": "/Final_Project/static/media/s4.750124ee.jpg"
+  },
+  {
+    "revision": "300c205ae0ad6bf455eb4ad04628244e",
+    "url": "/Final_Project/static/media/s5.300c205a.jpg"
+  }
+]);
